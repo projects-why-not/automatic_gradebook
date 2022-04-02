429ed567c06341f90bcec5954ae2f45cc844b208
lexmarkin <markinaleksandr386@gmail.com>
2022-04-02 16:57:07 +0300

    db commit db.py first

---
+++ vizual_system/db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+con = sqlite3.connect("gradebook")
+cur = con.cursor()
+
+
+def get_data(name_table, *parameters):
+    global cur
+    return cur.execute(f"""SELECT * FROM {name_table + " " + parameters[0]}""").fetchall()
+
+
+def add_data(name_table, *data):
+    global cur
+    cur.execute(f"""INSERT {name_table} VALUES {data}""")
+
+
+def view_tasks():
+    global cur
+    return cur.execute(f"""SELECT * FROM discipline_task""")
+
+
+def add_task(data):
+    add_data("discipline_task", data)
+
+
+def get_students_by_group(name_group):
+    str_param = f"WHERE class_name={name_group}"
+    return get_data("student_mapping_scheme", str_param)
+
+
+def add_student(name, class_name):
+    add_data("student_mapping_scheme", name, class_name)
+
+
